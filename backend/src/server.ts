68c8a5e413516044ea54877ddcfc747e90376f06
Milan Dankovic <dankovicmilan@gmail.com>
2022-06-14 18:17:23 +0200

    feat: connect to db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 const app = express();
+
+
+mongoose.connect('mongodb://localhost:27017/FiscalizationSystem');
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log('db connection ok')
+})
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(4000, () => console.log(`Express server running on port 4000`));
